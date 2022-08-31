f6e67874c35347dd78cf2d6df70f9f42187e2ca6
xiuyuan0216 <x1yuan@ucsd.edu>
2022-08-31 17:21:43 -0400

    add error_code_extract, correct error_code.json

---
+++ Error_code_extract.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from utils.json_load import *
+
+
+def Error_code_extract(event_log_relavant, SerialNo):
+    for index, row in event_log_relavant.iterrows():
+        try:
+            if row['Cartridge S/N'] == SerialNo and 'Cartridge removed' in row['Message']:
+                messageSplit = row['Message'].split()
+
+                # Grabs error code from event log message column
+                cartErrorCode = messageSplit[2]
+                cartErrorCode = cartErrorCode.rstrip(cartErrorCode[-1])
+                cartErrorCode = int(cartErrorCode)
+                
+            else:
+                cartErrorCode = 0
+
+        except IndexError:
+            print('Index Error Detected.')
+        except UnboundLocalError:
+            print('Cartridge probably ejected almost immediately.')
