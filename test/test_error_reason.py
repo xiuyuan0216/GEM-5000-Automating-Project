c42fce09d08d6d76dce0977dc03a810cf32fc6c6
xiuyuan0216 <x1yuan@ucsd.edu>
2022-09-01 00:59:58 -0400

    test error_reason_extract

--- test/test_error_reason.py
+++ test/test_error_reason.py
@@ -6,13 +6,11 @@ from Main_parse import *
 from Sensor_parse import *
 from Event_log_parse import *
 from Error_code_extract import *
-from Error_message_extract import *
+from Error_reason_extract import *
 
 copyIL = Select_copyIL()
 sensor_path, event_log_path = Main_parse(copyIL)
 sensor_file, serialNo = Sensor_parse(sensor_path)
 event_log_relavant = Event_log_parse(event_log_path, serialNo)
 error_code = Error_code_extract(event_log_relavant)
-print(Error_message_extract(error_code))
-
-
+print(Error_reason_extract(error_code))
